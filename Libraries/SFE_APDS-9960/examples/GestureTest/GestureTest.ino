813997a161fd6d8a4ef5fd686b3e405847f6a7c7
Shawn Hymel <shawn.hymel@sparkfun.com>
2014-05-30 15:24:14 -0600

    Added skeleton library

---
+++ Libraries/SFE_APDS-9960/examples/GestureTest/GestureTest.ino
@@ -0,0 +1,37 @@
+/****************************************************************
+GestureTest.ino
+APDS-9960 RGB and Gesture Sesnor
+Shawn Hymel @ SparkFun Electronics
+May 30, 2014
+https://github.com/sparkfun/APDS-9960_RGB_and_Gesture_Sensor
+
+Tests the gesture sensing abilities of the APDS-9960. Configures
+APDS-9960 over I2C and waits for gesture events. Calculates the
+direction of the swipe (up, down, left, right) and displays it
+on a serial console.
+
+Hardware Connections:
+
+IMPORTANT: The APDS-9960 can only accept 3.3V!
+ 
+ Arduino Pin  APDS-9960 Board  Function
+ 
+ 3.3V         VCC              Power
+ GND          GND              Ground
+ A4           SDA              I2C Data
+ A5           SCL              I2C Clock
+ 2            INT              Interrupt
+
+Resources:
+Include SFE_APDS-9960.h
+
+Development environment specifics:
+Written in Arduino 1.0.5
+Tested with SparkFun Arduino Pro Mini 3.3V
+
+This code is beerware; if you see me (or any other SparkFun 
+employee) at the local, and you've found our code helpful, please
+buy us a round!
+
+Distributed as-is; no warranty is given.
+****************************************************************/
